4475705ff5c53a067024287479cbe1130c2a3713
Henrp <henry1112200@gmail.com>
2023-08-18 15:31:29 -0700

    Added default profile image

--- src/components/Sidebar/UserInfo.tsx
+++ src/components/Sidebar/UserInfo.tsx
@@ -7,15 +7,15 @@ export default function UserInfo() {
       className="flex items-center justify-center px-4 py-2
     border-2 border-slate-300 rounded-2xl shadow-lg"
     >
-      {/* <div className="flex">
+      <div className="flex">
         <Image
           className="rounded-full"
-          src={}
-          width={}
-          height={}
-          alt="User Profile Pic"
+          src="public/default-profile-pic.jpeg"
+          width={30}
+          height={30}
+          alt="User Profile Picture"
         />
-      </div> */}
+      </div>
 
       <p className="text-xl">Guest</p>
     </div>
