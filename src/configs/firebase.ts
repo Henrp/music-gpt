51f4c537551baebccbc2c8d5ba4aee8d126bb863
retz8 <ekkicb71@gmail.com>
2023-09-25 00:06:33 -0400

    firebase config fixed

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { getApps, getApp, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDgAO-mQQKN-c1badFeBQ-5ldVxuJHFdTQ",
-  authDomain: "music-gpt-7194a.firebaseapp.com",
-  projectId: "music-gpt-7194a",
-  storageBucket: "music-gpt-7194a.appspot.com",
-  messagingSenderId: "387648943462",
-  appId: "1:387648943462:web:b5bef9bee06bad02ac44a7",
-  measurementId: "G-PVXN1Z2YVK",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
